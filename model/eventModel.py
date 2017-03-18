4f8e25c4a789baaad37bbad0cc536e924e593532
yenoss <cpg0504@gmail.com>
2017-03-18 13:09:35 +0900

    add cladav, google reccurence

--- model/eventModel.py
+++ model/eventModel.py
@@ -2,25 +2,25 @@ from common.util import utils
 from manager import db_manager
 
 #eventModel
-def setGoogleEvents(event_hashkey,calendar_hashkey,event_id,summary,start_date,end_date,created,updated,location):
+def setGoogleEvents(event_hashkey,calendar_hashkey,event_id,summary,start_date,end_date,created,updated,location,recurrence):
 	return db_manager.query(
 								"INSERT INTO EVENT " 
-								"(event_hashkey,calendar_hashkey,event_id,summary,start_dt,end_dt,created_dt,updated_dt,location) "
+								"(event_hashkey,calendar_hashkey,event_id,summary,start_dt,end_dt,created_dt,updated_dt,location,recurrence) "
 								"VALUES "
-								"(%s, %s, %s, %s, %s, %s, %s, %s, %s) ",
+								"(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ",
 								(			
-									event_hashkey,calendar_hashkey,event_id,summary,start_date,end_date,created,updated,location
+									event_hashkey,calendar_hashkey,event_id,summary,start_date,end_date,created,updated,location,recurrence
 								)
 							)
 #caldav
-def setCaldavEvents(event_hashkey,calendar_hashkey,event_id,summary,start_dt,end_dt,created_dt,updated_dt,location,caldav_event_url,caldav_etag):
+def setCaldavEvents(event_hashkey,calendar_hashkey,event_id,summary,start_dt,end_dt,created_dt,updated_dt,location,caldav_event_url,caldav_etag,recurrence):
 	return db_manager.query(
 								"INSERT INTO EVENT " 
-								"(event_hashkey,calendar_hashkey,event_id,summary,start_dt,end_dt,created_dt,updated_dt,location,caldav_event_url,caldav_etag) "
+								"(event_hashkey,calendar_hashkey,event_id,summary,start_dt,end_dt,created_dt,updated_dt,location,caldav_event_url,caldav_etag,recurrence) "
 								"VALUES "
-								"(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ",
+								"(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ",
 								(			
-									event_hashkey,calendar_hashkey,event_id,summary,start_dt,end_dt,created_dt,updated_dt,location,caldav_event_url,caldav_etag
+									event_hashkey,calendar_hashkey,event_id,summary,start_dt,end_dt,created_dt,updated_dt,location,caldav_event_url,caldav_etag,recurrence
 								)
 							)	
 
