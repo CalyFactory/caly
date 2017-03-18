5874f151bcd637137b24b7fe244504b7290ef2af
JSpiner <jspiner@naver.com>
2017-03-18 20:58:18 +0900

    add cron sync worker script

--- run_worker.sh
+++ run_worker.sh
@@ -1,1 +1,2 @@
 source envCaly/bin/activate && NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program celery -A sync_worker worker --loglevel=debug -f log_sync_worker.log
+source envCaly/bin/activate && NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program celery -A cron_worker worker --beat --loglevel=debug -f log_cron_worker.log
