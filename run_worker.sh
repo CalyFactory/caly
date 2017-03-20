b6c6c17f3a07c8454a70f1b019b7f17dfe4acc96
yenoss <cpg0504@gmail.com>
2017-03-20 18:12:50 +0900

    add python port run

--- run_worker.sh
+++ run_worker.sh
@@ -1,1 +1,1 @@
-source envCaly/bin/activate && NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program celery -A sync_worker worker --loglevel=debug -f log_sync_worker.log
+source envCaly/bin/activate && NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program celery -A sync_worker worker --loglevel=debug -f log/log_sync_worker.log
